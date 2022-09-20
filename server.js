6ae768d8d1252e3e6aa8e4e2b5947c747bb92cbe
Lee Tran <lee.tran95@yahoo.com>
2022-09-19 22:03:47 -0700

    Add config folder and install mongoose

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const { response } = require('express');
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/database');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
-const PORT = 8000
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
-require('dotenv').config();
+require('dotenv').config({path: './config/.env'})
 
-app.listen(process.env.PORT || PORT, () =>{
-    console.log(`server is running on ${PORT}`)
+connectDB()
+
+app.listen(process.env.PORT, () =>{
+    console.log(`server is running`)
 })
 
 let db,
