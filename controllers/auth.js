1396955099187a0e3826e46fcd04f8f09fafbdab
Lee Tran <lee.tran95@yahoo.com>
2023-02-03 01:53:04 -0800

    Fix issue where username was not being saved which led to user signup issues

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,17 @@ const validator = require('validator')
 const User = require('../models/Users')
 
 module.exports = {
+    logout: (req,res) =>{
+      req.logout(() => {
+        console.log('User has logged out.')
+      })
+      req.session.destroy((err) => {
+        if (err) console.log('Error : Failed to destroy the session during logout.', err)
+        req.session = null;
+        req.user = null
+        res.redirect('/')
+      })
+    },
     getLoginIndex: (req,res) =>{
         res.render('login.ejs')
     },
@@ -57,6 +68,7 @@ module.exports = {
     ]}, (err, existingUser) => {
       if (err) { return next(err) }
       if (existingUser) {
+        console.log('existing')
         req.flash('errors', { msg: 'Account with that email address or username already exists.' })
         return res.redirect('../signup')
       }
