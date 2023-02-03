5a59e4b50ee0a91b4ce2b65097e39e8c5aa9d7b1
Lee Tran <lee.tran95@yahoo.com>
2023-02-02 22:16:53 -0800

    Add sign in route and sign in controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,30 @@ module.exports = {
     getLoginIndex: (req,res) =>{
         res.render('login.ejs')
     },
+    postLogin: (req,res) =>{
+      const validationErrors = []
+      if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+      if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
+    
+      if (validationErrors.length) {
+        req.flash('errors', validationErrors)
+        return res.redirect('/login')
+      }
+      req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+    
+      passport.authenticate('local', (err, user, info) => {
+        if (err) { return next(err) }
+        if (!user) {
+          req.flash('errors', info)
+          return res.redirect('/login')
+        }
+        req.logIn(user, (err) => {
+          if (err) { return next(err) }
+          req.flash('success', { msg: 'Success! You are logged in.' })
+          res.redirect(req.session.returnTo || '/todos')
+        })
+      })(req, res, next)
+    },
     getSignUpIndex: (req,res) =>{
         res.render('signup.ejs')
     },
--- routes/home.js
+++ routes/home.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/auth')
 
 router.get('/',homeController.getIndex)
 router.get('/login',authController.getLoginIndex)
+router.post('/login', authController.postLogin)
 router.get('/signup',authController.getSignUpIndex)
 router.post('/signup',authController.postSignUp)
 
